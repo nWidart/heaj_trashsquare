c15301db8c1a83172d3107002d62ca1a5aa4cfb0
Nicolas Widart <n.widart@gmail.com>
2012-03-14 21:47:31 +0100

    db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -5,23 +5,12 @@
 // defined('DB_PASS')   ? null : define("DB_PASS", "root");
 // defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
 
-// defined('DB_SERVER') ? null : define("DB_SERVER", $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT'] );
-// defined('DB_USER')   ? null : define("DB_USER", $_SERVER['DB1_USER'] );
-// defined('DB_PASS')   ? null : define("DB_PASS", $_SERVER['DB1_PASS'] );
-// defined('DB_NAME')   ? null : define("DB_NAME", $_SERVER['DB1_NAME'] );
-
-if (isset($_SERVER['PLATFORM']) && $_SERVER['PLATFORM'] == 'PAGODABOX') {
 defined('DB_SERVER') ? null : define("DB_SERVER", $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT'] );
 defined('DB_USER')   ? null : define("DB_USER", $_SERVER['DB1_USER'] );
 defined('DB_PASS')   ? null : define("DB_PASS", $_SERVER['DB1_PASS'] );
 defined('DB_NAME')   ? null : define("DB_NAME", $_SERVER['DB1_NAME'] );
-}
-else {
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "root");
-defined('DB_PASS')   ? null : define("DB_PASS", "root");
-defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
-}
+
+
 // Create a database connection
 $connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
 if (!$connection) {
