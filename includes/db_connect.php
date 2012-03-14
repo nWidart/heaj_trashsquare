3cfe0c152af1eaba3b49da85eb69344a665b9ebd
Nicolas Widart <n.widart@gmail.com>
2012-03-14 21:13:33 +0100

    pagoda info

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,10 +1,14 @@
 <?php
 // Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "root");
-defined('DB_PASS')   ? null : define("DB_PASS", "root");
-defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
+// defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+// defined('DB_USER')   ? null : define("DB_USER", "root");
+// defined('DB_PASS')   ? null : define("DB_PASS", "root");
+// defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
 
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER')   ? null : define("DB_USER", "beverlee");
+defined('DB_PASS')   ? null : define("DB_PASS", "N4UE9urk");
+defined('DB_NAME')   ? null : define("DB_NAME", "lundie");
 // Create a database connection
 $connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
 if (!$connection) {
