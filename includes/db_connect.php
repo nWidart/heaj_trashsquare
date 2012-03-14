bb8449fd3ee0b571c4ddc1aafa027fb72dd3d183
Nicolas Widart <n.widart@gmail.com>
2012-03-14 21:22:04 +0100

    config 02

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -5,10 +5,10 @@
 // defined('DB_PASS')   ? null : define("DB_PASS", "root");
 // defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
 
-defined('DB_SERVER') ? null : define("DB_SERVER", "tunnel.pagodabox.com:3306");
-defined('DB_USER')   ? null : define("DB_USER", "beverlee");
-defined('DB_PASS')   ? null : define("DB_PASS", "N4UE9urk");
-defined('DB_NAME')   ? null : define("DB_NAME", "lundie");
+defined('DB_SERVER') ? null : define("DB_SERVER", $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT'] );
+defined('DB_USER')   ? null : define("DB_USER", $_SERVER['DB1_USER'] );
+defined('DB_PASS')   ? null : define("DB_PASS", $_SERVER['DB1_PASS'] );
+defined('DB_NAME')   ? null : define("DB_NAME", $_SERVER['DB1_NAME'] );
 // Create a database connection
 $connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
 if (!$connection) {
