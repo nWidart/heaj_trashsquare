80740bae5f09c6c711dc284dcb0b1fa89bcda169
Nicolas Widart <n.widart@gmail.com>
2012-03-15 11:42:07 +0100

    ajout du rank

--- param.php
+++ param.php
@@ -44,6 +44,7 @@ $donnees = mysql_fetch_array($reponse);
 			<img src="images/avatar.png" alt="avatar">
 			<h2><?php echo $userPrenom . " " . $userNom; ?></h2>
 			<p class="classe">2TiD1</p>
+			<p class="level"><?php echo get_the_titre( $score[1] ); ?></p>
 			<ul class="profile_menu">
 				<li class="param"><a href="param.php">Paramètres</a></li>
 				<li class="checkin"><a href="check.php">Check-in</a></li>
--- profil.php
+++ profil.php
@@ -15,7 +15,7 @@
 			<img src="images/avatar.png" alt="avatar">
 			<h2 class="nom"><?php echo $userPrenom . " " . $userNom; ?></h2>
 			<p class="classe">2TiD1</p>
-			<p class="level">Initié</p>
+			<p class="level"><?php echo get_the_titre( $score[1] ); ?></p>
 			<ul class="profile_menu">
 				<li class="param"><a href="param.php">Paramètres</a></li>
 				<li class="checkin"><a href="check.php">Check-in</a></li>
