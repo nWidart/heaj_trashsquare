fd434418fe3896fbd96ce6f317bd2de155022811
Nicolas Widart <n.widart@gmail.com>
2012-03-16 15:50:11 +0100

    le message

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,14 +1,14 @@
 <?php
 // Database Constants
-// defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-// defined('DB_USER')   ? null : define("DB_USER", "root");
-// defined('DB_PASS')   ? null : define("DB_PASS", "root");
-// defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER')   ? null : define("DB_USER", "root");
+defined('DB_PASS')   ? null : define("DB_PASS", "root");
+defined('DB_NAME')   ? null : define("DB_NAME", "trashsquare");
 
-defined('DB_SERVER') ? null : define("DB_SERVER", $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT'] );
-defined('DB_USER')   ? null : define("DB_USER", $_SERVER['DB1_USER'] );
-defined('DB_PASS')   ? null : define("DB_PASS", $_SERVER['DB1_PASS'] );
-defined('DB_NAME')   ? null : define("DB_NAME", $_SERVER['DB1_NAME'] );
+// defined('DB_SERVER') ? null : define("DB_SERVER", $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT'] );
+// defined('DB_USER')   ? null : define("DB_USER", $_SERVER['DB1_USER'] );
+// defined('DB_PASS')   ? null : define("DB_PASS", $_SERVER['DB1_PASS'] );
+// defined('DB_NAME')   ? null : define("DB_NAME", $_SERVER['DB1_NAME'] );
 
 
 // Create a database connection
