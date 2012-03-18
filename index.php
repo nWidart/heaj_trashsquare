89a6b3ccd89faf6a10baab0a97f014fe0a634d0b
Nicolas Widart <n.widart@gmail.com>
2012-03-18 22:36:09 +0100

    ajout d'index

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php $page_title = "Trashsquare | Index"; ?>
 <?php include('includes/header.php'); ?>
 
-<div class="container contenu">
+<div class="container contenu index">
 	<div class="row">
-		<div class="index">
+		<div class="ninecol">
 			<h2>TU JETTES, TU CHECK ET TU RESPECTES…</h2>
+			<p>Quel est le point commun entre une école pleine de déchets, un code barre, et une fresque ? Trashsquare ! Qu'est-ce que c'est que cet oiseau-là ?</p>
+			<p>Dans quelques jours vous verrez déambuler dans l'école de drôles de gens, arborant fièrement leur projet (photo t-shirt, arrive demain) dans le dos et vous incitant à aller jeter vos canettes, papiers et autres objets indésirables dans leur poubelle (photo, idem), n'ayez pas peur ! Non, ce n'est pas une secte, et ils sont moins effrayant qu'ils n'en ont l'air.</p>
+
+			<p>Trashsquare c'est une idée simple : rendre ludique le fait de jeter ses déchets. Vous connaissez tous Foursquare qui vous permet de devenir roi du monde (bon d'accord, maire d'un lieu unique, mais l'idée est là) à coups de check-in. Nous adaptons ce principe en créant Trashsquare : une poubelle à votre disposition et la possibilité de devenir roi de la propreté !"</p>
 		</div>
+		<div class="twocol last">
+			<h3>Nous suivre...</h3>
+			<ul>
+				<li><a href="https://twitter.com/#!/Trashsquare" class="twitter"></a></li>
+				<li><a href="https://www.facebook.com/Trashsquare" class="facebook"></a></li>
+			</ul>
+		</div>	
 	</div>
 </div>
 
