a66e032da687a75846e7c98290dcffb5f30efdfa
Nicolas Widart <n.widart@gmail.com>
2012-03-18 23:34:58 +0100

    typo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 			<p>Trashsquare c'est une idée simple : rendre ludique le fait de jeter ses déchets. Vous connaissez tous Foursquare qui vous permet de devenir roi du monde (bon d'accord, maire d'un lieu unique, mais l'idée est là) à coups de check-in. Nous adaptons ce principe en créant Trashsquare : une poubelle à votre disposition et la possibilité de devenir roi de la propreté !</p>
 			<h2>Fonctionnement de trashsquare</h2>
 			<p>Ce mardi 20 mars 2012 vous serez accueilli par des membres de l'équipe Trashsquare aux alentours des poubelles à forte fréquentation de l'ESIAJ : Grand hall, Bâtiment A, B supérieur et C.</p>
-			<p>Il vous sera remis lors de votre première utilisation des poubelles Trashsquar un autocollant à placer à l'arrière de votre carte étudiant, contenant vos identifiants uniques.</p>
+			<p>Il vous sera remis lors de votre première utilisation des poubelles Trashsquare un autocollant à placer à l'arrière de votre carte étudiant, contenant vos identifiants uniques.</p>
 			<p>Une fois votre déchet jeté, votre compte sera crédité d'un check-in ! Une seule adresse alors : <a href="www.trashsquare.be">trashsquare.be</a> ! Vous aurez la possibilité d'y compléter votre compte. Plus vous possédez de check-in, plus votre grade augmente.</p>
 			<p>À votre tour de devenir le roi !</p>
 		</div>
