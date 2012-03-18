2a51ea7b7f98c36047dcbcff3c700a426116e411
Nicolas Widart <n.widart@gmail.com>
2012-03-18 22:45:02 +0100

    index, texte partie 2

--- index.php
+++ index.php
@@ -4,14 +4,19 @@
 <div class="container contenu index">
 	<div class="row">
 		<div class="ninecol">
-			<h2>TU JETTES, TU CHECK ET TU RESPECTES…</h2>
+			<h2>Tu jettes, tu check et tu respectes …</h2>
 			<p>Quel est le point commun entre une école pleine de déchets, un code barre, et une fresque ? Trashsquare ! Qu'est-ce que c'est que cet oiseau-là ?</p>
 			<p>Dans quelques jours vous verrez déambuler dans l'école de drôles de gens, arborant fièrement leur projet (photo t-shirt, arrive demain) dans le dos et vous incitant à aller jeter vos canettes, papiers et autres objets indésirables dans leur poubelle (photo, idem), n'ayez pas peur ! Non, ce n'est pas une secte, et ils sont moins effrayant qu'ils n'en ont l'air.</p>
 
 			<p>Trashsquare c'est une idée simple : rendre ludique le fait de jeter ses déchets. Vous connaissez tous Foursquare qui vous permet de devenir roi du monde (bon d'accord, maire d'un lieu unique, mais l'idée est là) à coups de check-in. Nous adaptons ce principe en créant Trashsquare : une poubelle à votre disposition et la possibilité de devenir roi de la propreté !"</p>
+			<h2>Fonctionement de trashsquare</h2>
+			<p>Ce mardi 20 mars 2012 vous serez accueilli par des membres de l'équipe Trashsquare aux alentours des poubelles à forte fréquentation de l'ESIAJ : Grand hall, Bâtiment A, B supérieur et C.</p>
+			<p>Il vous sera remis lors de votre première utilisation des poubelles Trashsquar un autocollant à placer à l'arrière de votre carte étudiant, contenant vos identifiants uniques.</p>
+			<p>Une fois votre déchet jeté, votre compte sera crédité d'un check-in ! Une seule adresse alors : <a href="www.trashsquare.be">trashsquare.be</a>! Vous aurez la possibilité d'y compléter votre compte. Plus vous possédez de check-in, plus votre grade augmente.</p>
+			<p>À votre tour de devenir le roi !</p>
 		</div>
 		<div class="twocol last">
-			<h3>Nous suivre...</h3>
+			<h3>Nous suivre ...</h3>
 			<ul>
 				<li><a href="https://twitter.com/#!/Trashsquare" class="twitter"></a></li>
 				<li><a href="https://www.facebook.com/Trashsquare" class="facebook"></a></li>
