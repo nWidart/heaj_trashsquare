43b57954421fcdfb901b19a14ec84fa9f2ab26a1
Nicolas Widart <n.widart@gmail.com>
2012-03-19 21:44:23 +0100

    style index

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 	<div class="row">
 		<div class="ninecol">
 			<h2>Tu jettes, tu checkes et tu respectes …</h2>
-			<p>Quel est le point commun entre une école pleine de déchets, un code barre, et une fresque ? Trashsquare ! Qu'est-ce que cet <a href="https://twitter.com/#!/Trashsquare">oiseau-là</a> ?</p>
+			<p>Quel est le point commun entre une école pleine de déchets, un code barre, et une fresque ? <strong>Trashsquare</strong> ! Qu'est-ce que cet <a href="https://twitter.com/#!/Trashsquare">oiseau-là</a> ?</p>
 			<p>Dans quelques jours vous verrez déambuler dans l'école de drôles de gens, arborant fièrement leur projet dans le dos et vous incitant à aller jeter vos canettes, papiers et autres objets indésirables dans leur poubelle , n'ayez pas peur ! Non, ce n'est pas une secte, et ils sont moins effrayants qu'ils n'en ont l'air.</p>
 
 			<p>Trashsquare c'est une idée simple : rendre ludique le fait de jeter ses déchets. Vous connaissez tous Foursquare qui vous permet de devenir roi du monde à coups de check-in. Nous adaptons ce principe en créant Trashsquare : une poubelle à votre disposition et la possibilité de devenir roi de la propreté !</p>
 			<h2>Fonctionnement de trashsquare</h2>
-			<p>Ce mardi 20 mars 2012 vous serez accueillis par des membres de l'équipe Trashsquare aux alentours des poubelles à forte fréquentation de l'ESIAJ : Grand hall, Bâtiment A, B supérieur et le B99.</p>
+			<p>Ce mardi 20 mars 2012 vous serez accueillis par des membres de l'équipe Trashsquare aux alentours des poubelles à forte fréquentation de l'ESIAJ : <strong>Grand hall</strong>, <strong>Bâtiment A</strong>, <strong>B supérieur</strong> et le <strong>B99</strong>.</p>
 			<p>Il vous sera remis lors de votre première utilisation des poubelles Trashsquare un autocollant à placer à l'arrière de votre carte étudiant, contenant vos identifiants uniques.</p>
 			<p>Une fois votre déchet jeté, votre compte sera crédité d'un check-in ! Une seule adresse alors : <a href="http://www.trashsquare.be">trashsquare.be</a> ! Vous aurez la possibilité d'y compléter votre compte. Plus vous possédez de check-in, plus votre grade augmente.</p>
 			<p>À votre tour de devenir le roi !</p>
