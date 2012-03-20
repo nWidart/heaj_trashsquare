2c6fc047b5185aed66ead0da4c858786443aa13a
Nicolas Widart <n.widart@gmail.com>
2012-03-20 10:00:27 +0100

    suite de la mise en place des ranks

--- param.php
+++ param.php
@@ -103,46 +103,47 @@ $donnees = mysql_fetch_array($reponse);
 		</div>
 		<div class="threecol last">
                <?php 
-				if ( isset($userId) ) {
-               ?>
-			<h2>Badges reçus</h2>
-			<?php 
-			if ( $score > 1 ) { ?>
-			<div class="grade first">
-				<img src="images/lvl2.png" alt="lvl2" />
-				<p><span>Initié</span>
-				Tu as utilisé les poubelles de recyclage.</p>
-			</div>
-			<?php } if ( $score > 2 ) { ?>
-			<div class="grade">
-				<img src="images/lvl1.png" alt="lvl1" />
-				<p><span>Débutant</span>
-				Tu as jeté 5 déchets durant les heures de cours.</p>
-			</div>
-			<?php } if ( $score > 4 ) { ?>
-			<div class="grade">
-				<img src="images/lvl3.png" alt="lvl3" />
-				<p><span>Explorateur</span>
-				Tu as jeté plus de 10 déchets au total.</p>
-			</div>
-			<?php } if ( $score > 8 ) { ?>
-			<div class="grade">
-				
-				<img src="images/lvl4.png" alt="lvl4" />
-				<p><span>Aventurier</span>
-				Tu as jeté plus de 50 déchets au total.</p>
-			</div>
-			<?php } if ( $score > 12 ) { ?>
-			<div class="grade">
-				<img src="images/lvl5.png" alt="lvl5" />
-				<p><span>Mr. Propre</span>
-				Tu as jeté plus de 75 déchets au total.</p>
-			</div>
-			<?php } ?>
-			
-           <?php 
-       		} 
-       		?>
+					if ( isset($userId) ) {
+				   ?>
+				<h2>Badges reçus</h2>
+
+				<?php 
+				if ( $score[1] > 1 ) { ?>
+				<div class="grade first">
+					<img src="images/lvl2.png" alt="lvl2" />
+					<p><span>Initié</span>
+					Tu as utilisé les poubelles de recyclage.</p>
+				</div>
+				<?php } if ( $score[1] > 2 ) { ?>
+				<div class="grade">
+					<img src="images/lvl1.png" alt="lvl1" />
+					<p><span>Débutant</span>
+					Tu as jeté 2 déchets durant les heures de cours.</p>
+				</div>
+				<?php } if ( $score[1] > 4 ) { ?>
+				<div class="grade">
+					<img src="images/lvl3.png" alt="lvl3" />
+					<p><span>Explorateur</span>
+					Tu as jeté plus de 4 déchets au total.</p>
+				</div>
+				<?php } if ( $score[1] > 8 ) { ?>
+				<div class="grade">
+					
+					<img src="images/lvl4.png" alt="lvl4" />
+					<p><span>Aventurier</span>
+					Tu as jeté plus de 8 déchets au total.</p>
+				</div>
+				<?php } if ( $score[1] > 12 ) { ?>
+				<div class="grade">
+					<img src="images/lvl5.png" alt="lvl5" />
+					<p><span>Mr. Propre</span>
+					Tu as jeté plus de 12 déchets au total.</p>
+				</div>
+				<?php } ?>
+
+				<?php 
+					} 
+					?>
 		</div>
 	</div>
 </div>
